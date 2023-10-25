c4c420feae6ab8759510e5a0f9370fd7d9875fc5
Riya Bhatia <riyab0504@gmail.com>
2023-10-25 12:49:00 -0400

    fix code formatting

--- app/tests/test_fib.py
+++ app/tests/test_fib.py
@@ -1,7 +1,10 @@
 from app.fib import fibonacci
 
 # Test cases for the fibonacci function
-def test_first_fibonacci(): assert(fibonacci(1) == 1)
+def test_first_fibonacci():
+    assert fibonacci(1) == 1
+
+
 def test_zero_fibonacci():
     try:
         fibonacci(0)
