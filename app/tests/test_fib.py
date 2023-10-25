31b640311a860781642d2ce7ae426e1b4ded4e12
Riya Bhatia <riyab0504@gmail.com>
2023-10-25 12:32:09 -0400

    fix test to raise ValueError

--- app/tests/test_fib.py
+++ app/tests/test_fib.py
@@ -2,4 +2,4 @@ from app.fib import fibonacci
 
 # Test cases for the fibonacci function
 def test_first_fibonacci(): assert(fibonacci(1) == 1)
-def test_zero_fibonacci(): assert(fibonacci(0) == ValueError)
+def test_zero_fibonacci(): assert(fibonacci(0) == ValueError("Invalid input"))
